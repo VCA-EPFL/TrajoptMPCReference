9948d5c7cac358b8f2616a059d8fcd25af03038f
margg00 <margueritebenoist@gmail.com>
2024-02-23 16:39:05 +0100

    Cost for 2 link arm to reach goal ee position

--- TrajoptCost.py
+++ TrajoptCost.py
@@ -1,5 +1,6 @@
 import numpy as np
-from sympy import symbols, diff, Matrix, cos, sin
+from sympy import symbols, diff, Matrix, cos, sin, MatrixSymbol, BlockMatrix
+
 
 class TrajoptCost:
 	def value():
@@ -42,6 +43,7 @@ class QuadraticCost(TrajoptCost):
 		delta_x = x - self.xg
 		currQ = self.get_currQ(u,timestep)
 		top = np.matmul(delta_x.transpose(),currQ)
+
 		if u is None:
 			return top
 		else:
@@ -52,6 +54,7 @@ class QuadraticCost(TrajoptCost):
 		nx = self.Q.shape[0]
 		nu = self.R.shape[0]
 		currQ = self.get_currQ(u,timestep)
+
 		if u is None:
 			return currQ
 		else:
@@ -79,7 +82,7 @@ class QuadraticCost(TrajoptCost):
 		self.QF_start += shift
 		self.QF_start = max(self.QF_start, 0)
 		return self.QF_start
-	
+
 class ArmCost(TrajoptCost):
 
 	def __init__(self, Q_in: np.ndarray, QF_in: np.ndarray, R_in: np.ndarray, xg_in: np.ndarray, QF_start = None):
@@ -91,55 +94,116 @@ class ArmCost(TrajoptCost):
 		self.increaseCount_QF = 0
 		self.QF_start = QF_start
 		self.l1=1
-		self.l2=2
+		self.l2=1
+		self.cost_control_in=self.symbolic_cost(control=True)
+		self.cost_control_off=self.symbolic_cost(control=False)
+		self.grad_control_in=self.symbolic_gradient(control=True)
+		self.grad_control_off=self.symbolic_gradient(control=False)
+		self.hess_control_in=self.symbolic_hessian(control=True)
+		self.hess_control_off=self.symbolic_hessian(control=False)
+
+
+
+
+	def symbolic_cost(self, control=True):
+
+		q1,q2,q1_dot,q2_dot, u1, u2  = symbols('q1 q2 q1_dot q2_dot u1 u2')
+		Q= MatrixSymbol('Q',4,4)
+		# x=(self.l2*cos(q2)+self.l1)*cos(q1)
+		# y=(self.l2*cos(q2)+self.l1)*sin(q1)
+
+		x=self.l2*cos(q2+q1)+self.l1*cos(q1)
+		y=self.l2*sin(q2+q1)+self.l1*sin(q1)
+
+		
+		J = Matrix([
+			[-self.l2*sin(q2+q1)-self.l1*sin(q1), -self.l2*sin(q2+q1)],
+			[self.l2*cos(q2+q1)-self.l1*cos(q1), self.l2*cos(q2+q1)]
+		])
+		#Jacobian=Matrix([[diff(x,q1),diff(x,q2)],[diff(y,q1),diff(y,q2)]])
+		pos_ee=Matrix([x,y])
+		vel_ee=J@Matrix([q1_dot,q2_dot])
+		delta_x = Matrix([pos_ee, vel_ee]) - Matrix(self.xg)
+		print("delta \n:", Matrix(self.xg))
+		cost=0.5*delta_x.T@Q@delta_x
+ 
+		if control:
+			cost += 0.5 * Matrix([u1,u2]).T * self.R * Matrix([u1,u2])
+
+		return cost
 
 	def value(self, x: np.ndarray, u: np.ndarray = None, timestep: int = None):
-
-		[q1,q2] = x[:2]
+		q1, q2, q1_dot, q2_dot, u1, u2= symbols('q1 q2 q1_dot q2_dot u1 u2')
+		Q= MatrixSymbol('Q',4,4)
 		currQ = self.get_currQ(u,timestep)
-		pos_ee=[(self.l2*cos(q2)+self.l1)*cos(q1),(self.l2*cos(q2)+self.l1)*sin(q1)]
-		delta_x = pos_ee - self.xg
-		cost = 0.5*np.matmul(delta_x.transpose(),np.matmul(currQ,delta_x))
-		if not isinstance(u, type(None)):
-			cost += 0.5*np.matmul(u.transpose(),np.matmul(self.R,u))
-		return cost
-	
-
-	def gradient(self,l1,l2,x: np.ndarray, u: np.ndarray = None, timestep: int = None):
-		q1,q2,u1,u2 = symbols('q1 q2 u1 u2')
-		currQ = self.get_currQ(u,timestep)
-		cost=self.symbolic_cost(q1,q2,u1,u2,currQ)
-		if u is None:
-			gradient = Matrix([diff(cost, q1), diff(cost, q2), diff(cost, u1), diff(cost, u2)])
-			return gradient.subs({q1: x[0], q2: x[1]})
-		else:
-			gradient = Matrix([diff(cost, q1), diff(cost, q2), diff(cost, u1), diff(cost, u2)])
-			return gradient.subs({q1: x[0], q2: x[1],q1: u[0], q2: u[1]})
-		
-		
-	
-	def hessian(self,l1,l2,x: np.ndarray, u: np.ndarray = None, timestep: int = None):
-		q1,q2,u1,u2 = symbols('q1 q2 u1 u2')
-		currQ = self.get_currQ(u,timestep)
-		cost=self.symbolic_cost(q1,q2,u1,u2, currQ)
 
 		if u is None:
-			hessian = Matrix([[diff(cost, q1, q1), diff(cost, q1, q2)],
-                          [diff(cost, q2, q1), diff(cost, q2, q2)]])
-			return hessian.subs({q1: x[0], q2: x[1]})
+			symbolic_cost=self.cost_control_off
+			cost_value= symbolic_cost.subs({q1: x[0], q2: x[1],q1_dot: x[2], q2_dot: x[3], Q:Matrix(currQ)})[0,0]
 		else:
-			hessian = Matrix([[diff(cost, q1_, q2_) for q1_ in [q1, q2, u1, u2]] for q2_ in [q1, q2, u1, u2]])
-			return hessian.subs({q1: x[0], q2: x[1],q1: u[0], q2: u[1]})
+			symbolic_cost=self.cost_control_in
+			cost_value= symbolic_cost.subs({q1: x[0], q2: x[1],q1_dot: x[2], q2_dot: x[3], u1: u[0], u2: u[1],Q:Matrix(currQ)})[0,0]
 		
-
-	def symbolic_cost(self,q1,q2,u1,u2,Q):
-		pos_ee=[(self.l2*cos(q2)+self.l1)*cos(q1),(self.l2*cos(q2)+self.l1)*sin(q1)]
-		delta_x = Matrix(pos_ee) - Matrix(self.xg)
-		cost=0.5*delta_x.T*Q*delta_x +0.5 * Matrix([u1,u2]).T * self.R * Matrix([u1,u2]) 
-		return cost
+		return cost_value
 
 
-	
+	def symbolic_gradient(self,control=True):
+		q1, q2, q1_dot, q2_dot, u1, u2= symbols('q1 q2 q1_dot q2_dot u1 u2')
+
+		if(control):
+			cost=self.cost_control_in
+			gradient = Matrix(BlockMatrix([diff(cost, q1), diff(cost, q2), diff(cost, q1_dot), diff(cost, q2_dot), diff(cost, u1), diff(cost, u2)]))
+			return gradient
+		else:
+			cost=self.cost_control_off
+			gradient = Matrix(BlockMatrix([diff(cost, q1), diff(cost, q2), diff(cost, q1_dot), diff(cost, q2_dot)]))
+			return gradient
+
+	def gradient(self,x: np.ndarray, u: np.ndarray = None, timestep: int = None):
+
+		q1, q2, q1_dot, q2_dot, u1, u2= symbols('q1 q2 q1_dot q2_dot u1 u2')
+		Q= MatrixSymbol('Q',4,4)
+		currQ = self.get_currQ(u,timestep)
+
+		if u is None:
+			symbolic_grad=self.grad_control_off
+			gradient_val=np.array(symbolic_grad.subs({q1: x[0], q2: x[1],q1_dot: x[2], q2_dot: x[3], Q:Matrix(currQ)})).astype(float)
+		else:
+			symbolic_grad=self.grad_control_in
+			gradient_val=np.array(symbolic_grad.subs({q1: x[0], q2: x[1],q1_dot: x[2], q2_dot: x[3], u1: u[0], u2: u[1], Q:Matrix(currQ)})).astype(float)
+
+		return gradient_val
+
+
+
+	def symbolic_hessian(self, control=True):
+		q1, q2, q1_dot, q2_dot, u1, u2= symbols('q1 q2 q1_dot q2_dot u1 u2')
+		if(control):
+			cost=self.cost_control_in
+			hessian = Matrix(BlockMatrix([[diff(cost, q1_, q2_) for q1_ in [q1, q2, q1_dot, q2_dot, u1, u2]] for q2_ in [q1, q2, q1_dot, q2_dot, u1, u2]]))
+			return hessian
+		else:
+			cost=self.cost_control_off
+			hessian= Matrix(BlockMatrix([[diff(cost, q1_, q2_) for q1_ in [q1, q2, q1_dot, q2_dot]] for q2_ in [q1, q2, q1_dot, q2_dot]]))
+			return hessian
+
+	def hessian(self,x: np.ndarray, u: np.ndarray = None, timestep: int = None):
+		q1, q2, q1_dot, q2_dot, u1, u2= symbols('q1 q2 q1_dot q2_dot u1 u2')
+		Q= MatrixSymbol('Q',4,4)
+		currQ = self.get_currQ(u,timestep)
+		hessian=self.hess_control_in
+		if u is None:
+			hessian=self.hess_control_off
+			hessian_evaluated = np.array(hessian.subs({q1: x[0], q2: x[1], q1_dot: x[2], q2_dot: x[3], Q:Matrix(currQ)})).astype(float)
+		else:
+			hessian=self.hess_control_in
+			hessian_evaluated =np.array(hessian.subs({q1: x[0], q2: x[1],q1_dot: x[2], q2_dot: x[3], u1: u[0], u2: u[1], Q:Matrix(currQ)})).astype(float)
+		
+		return hessian_evaluated
+
+
+
+
 	def get_currQ(self, u = None, timestep = None):
 		last_state = isinstance(u,type(None))
 		shifted_QF = (not isinstance(timestep,type(None)) \
@@ -148,7 +212,8 @@ class ArmCost(TrajoptCost):
 		use_QF =  last_state or shifted_QF
 		currQ = self.QF if use_QF else self.Q
 		return currQ
-	
+
+
 	def increase_QF(self, multiplier: float = 2.0):
 		self.QF *= multiplier
 		self.increaseCount_QF += 1
--- examples/2_link.py
+++ examples/2_link.py
@@ -5,18 +5,20 @@ sqp_solver_methods = [SQPSolverMethods.PCG_SS]#["N", "S", "PCG-J", "PCG-BJ", "PC
 mpc_solver_methods = [MPCSolverMethods.QP_PCG_SS] #["iLQR", "QP-N", "QP-S", "QP-PCG-J", "QP-PCG-BJ", "QP-PCG-SS"]
 
 options= {}
-options['path_to_urdf'] = '/home/tbourgea/git/OurClone/examples/2_link.urdf'
+options['path_to_urdf'] = '/home/marguerite/Documents/lab/TrajoptMPCReference/examples/2_link.urdf'
 plant = URDFPlant(options=options)
 
 
 N = 20
 dt = 0.1
 
-Q = np.diag([1.0,1.0, 1.0, 1.0])
-QF = np.diag([100.0,100.0, 100., 100.])
-R = np.diag([0.1, 0.1])
-xg = np.array([1.2,1.23,0,0])
-cost = QuadraticCost(Q,QF,R,xg)
+Q = np.diag([1.0,1.0,1.0,1.0])
+QF = np.diag([100.0,100.0,100.0,100.0])
+R = np.diag([0.1, 0.1]) 
+xg= np.array([0.,1.46,0.,0.])
+#cost = QuadraticCost(Q,QF,R,xg)
+cost = ArmCost(Q,QF,R,xg)
+
 
 hard_constraints = TrajoptConstraint(plant.get_num_pos(),plant.get_num_vel(),plant.get_num_cntrl(),N)
 hard_constraints.set_torque_limits([7.0],[-7.0],"ACTIVE_SET")
@@ -24,7 +26,7 @@ hard_constraints.set_torque_limits([7.0],[-7.0],"ACTIVE_SET")
 soft_constraints = TrajoptConstraint(plant.get_num_pos(),plant.get_num_vel(),plant.get_num_cntrl(),N)
 soft_constraints.set_torque_limits([7.0],[-7.0],"AUGMENTED_LAGRANGIAN")
 
-options = {"expected_reduction_min_SQP_DDP":-100, "display": True, "gravity" : -27} # needed for hard_constraints - TODO debug why
+options = {"expected_reduction_min_SQP_DDP":-1000, "display": True, "gravity" : -27} # needed for hard_constraints - TODO debug why
 
 runSQPExample(plant, cost, hard_constraints, soft_constraints, N, dt, sqp_solver_methods, options)
 
